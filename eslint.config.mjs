f4d728f16a291e5b3f67752a9ace978e83d380eb
Railly <railly_2009@hotmail.com>
2026-04-05 22:30:37 -0500

    chore: switch to eslint-plugin-obsidianmd, add codemirror dev deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
-import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import obsidianPlugin from "eslint-plugin-obsidian";
+import { defineConfig } from "eslint/config";
+import obsidianmd from "eslint-plugin-obsidianmd";
 
-export default [
+export default defineConfig([
+	...obsidianmd.configs.recommended,
 	{
 		files: ["src/**/*.ts"],
 		languageOptions: {
@@ -10,20 +11,35 @@ export default [
 			parserOptions: {
 				project: "./tsconfig.json",
 			},
-		},
-		plugins: {
-			"@typescript-eslint": tsPlugin,
-			obsidian: obsidianPlugin,
+			globals: {
+				document: "readonly",
+				navigator: "readonly",
+				setTimeout: "readonly",
+				clearTimeout: "readonly",
+				setInterval: "readonly",
+				clearInterval: "readonly",
+				DOMParser: "readonly",
+				NodeJS: "readonly",
+				process: "readonly",
+				Buffer: "readonly",
+				createEl: "readonly",
+			},
 		},
 		rules: {
-			...obsidianPlugin.rules
-				? Object.fromEntries(
-						Object.keys(obsidianPlugin.rules).map((r) => [`obsidian/${r}`, "error"])
-				  )
-				: {},
+			"import/no-extraneous-dependencies": ["error", {
+				devDependencies: true,
+				peerDependencies: true,
+				optionalDependencies: false,
+			}],
+			"@typescript-eslint/no-unsafe-assignment": "off",
+			"@typescript-eslint/no-unsafe-member-access": "off",
+			"@typescript-eslint/no-unsafe-argument": "off",
+			"@typescript-eslint/no-unsafe-call": "off",
+			"@typescript-eslint/no-unsafe-return": "off",
+			"@typescript-eslint/no-base-to-string": "off",
 		},
 	},
 	{
 		ignores: ["main.js", "node_modules/**", "*.mjs"],
 	},
-];
+]);
